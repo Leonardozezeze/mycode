06c5334cf2ae81852a6cfab3662917563d523024
Leonardozezeze <leonardozezeze@outlook.com>
2026-08-31 08:17:42 +0800

    学习rust

---
+++ rust/word_in_string/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let s=String::from("Hello World!");
+    let word=first_word(&s);
+    println!("{}",word);
+}
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
